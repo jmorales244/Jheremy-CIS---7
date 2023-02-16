e8f6d5c05d4c6962a80a9c0e622e6aafd6951297
Jheremy <95850594+jmorales244@users.noreply.github.com>
2023-02-15 20:20:33 -0800

    Initial commit

---
+++ CppApplication_2/main.cpp
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: 2929135
+ *
+ * Created on February 15, 2023, 7:33 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "Hello World!" << endl;
+    
+    string s = "My name is Jheremy!\n";
+    
+    cout << s << endl;
+    
+    string input; 
+    cout << "What is your name?:";
+    cin >> input;
+    
+    
+    
+    return 0;
+}
+
